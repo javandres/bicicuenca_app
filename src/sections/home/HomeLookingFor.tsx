9563dd67008339133df6d3af7e513d1446d2c0a0
Javier Andres GArcia <javier.garcia@foodgraph.com>
2023-02-07 19:19:29 -0500

    init

---
+++ src/sections/home/HomeLookingFor.tsx
@@ -0,0 +1,117 @@
+import { m } from 'framer-motion';
+// @mui
+import { styled } from '@mui/material/styles';
+import { Box, Button, Container, Typography, Grid, Stack } from '@mui/material';
+// next
+import NextLink from 'next/link';
+// hooks
+import useResponsive from '../../hooks/useResponsive';
+// routes
+import { PATH_DASHBOARD, PATH_ZONE_ON_STORE } from '../../routes/paths';
+// components
+import Image from '../../components/image';
+import Iconify from '../../components/iconify';
+import { MotionViewport, varFade } from '../../components/animate';
+
+// ----------------------------------------------------------------------
+
+const StyledRoot = styled('div')(({ theme }) => ({
+  padding: theme.spacing(10, 0),
+  [theme.breakpoints.up('md')]: {
+    padding: theme.spacing(15, 0),
+  },
+}));
+
+// ----------------------------------------------------------------------
+
+export default function HomeLookingFor() {
+  const isDesktop = useResponsive('up', 'md');
+
+  return (
+    <StyledRoot>
+      <Container component={MotionViewport}>
+        <Grid
+          container
+          alignItems="center"
+          justifyContent="space-between"
+          spacing={{ xs: 5, md: 0 }}
+        >
+          <Grid item xs={12} md={4}>
+            <Description />
+          </Grid>
+
+          <Grid item xs={12} md={7}>
+            <Content />
+          </Grid>
+
+          {!isDesktop && (
+            <Grid item xs={12} sx={{ textAlign: 'center' }}>
+              {VisitButton}
+            </Grid>
+          )}
+        </Grid>
+      </Container>
+    </StyledRoot>
+  );
+}
+
+// ----------------------------------------------------------------------
+
+function Description() {
+  const isDesktop = useResponsive('up', 'md');
+
+  return (
+    <Stack
+      sx={{
+        textAlign: {
+          xs: 'center',
+          md: 'left',
+        },
+      }}
+    >
+      <m.div variants={varFade().inDown}>
+        <Typography variant="overline" component="div" sx={{ color: 'text.disabled' }}>
+          Proyecto de análisis de datos y apoyo al sistema de bicicletas compartidas de
+        </Typography>
+      </m.div>
+
+      <m.div variants={varFade().inDown}>
+        <Typography
+          variant="h2"
+          sx={{
+            mt: 3,
+            mb: { md: 5 },
+          }}
+        >
+          Cuenca
+        </Typography>
+      </m.div>
+
+      {isDesktop && <m.div variants={varFade().inDown}> {VisitButton} </m.div>}
+    </Stack>
+  );
+}
+
+// ----------------------------------------------------------------------
+
+function Content() {
+  return (
+    <Box component={m.div} variants={varFade().inUp}>
+      <Image disabledEffect alt="rocket" src="/assets/images/home/zone_landing.png" />
+    </Box>
+  );
+}
+
+const VisitButton = (
+  <Button
+    color="inherit"
+    size="large"
+    variant="outlined"
+    rel="noopener"
+    component={NextLink}
+    href={PATH_DASHBOARD.root}
+    endIcon={<Iconify icon="ic:round-arrow-right-alt" />}
+  >
+    Entrar
+  </Button>
+);
